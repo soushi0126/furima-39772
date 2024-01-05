b18cdaf072257c9b25d9eb7bcd34e74b007a509e
soushi0126 <sousi0126@gmail.com>
2024-01-05 12:39:57 +0900

    バリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :fast_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :fast_name_kana, presence: true
+  validates :date_of_birth, presence: true
+
+  
 end
