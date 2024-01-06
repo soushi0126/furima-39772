63530ded95f56d1eef34baa06fdf861e7c6b3c15
soushi0126 <sousi0126@gmail.com>
2024-01-06 17:06:24 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :date_of_birth, presence: true
+  with_options presence: true do
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カナ文字を使用してください' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カナ文字を使用してください' }
+    validates :date_of_birth
+  end
 
 end
