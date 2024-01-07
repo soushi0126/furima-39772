5d8c12ff6c02f9634196fc315b3d089c8c2d7462
soushi0126 <sousi0126@gmail.com>
2024-01-07 12:28:59 +0900

    バリーデーションをリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,16 @@ class User < ApplicationRecord
     validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カナ文字を使用してください' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カナ文字を使用してください' }
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナ文字を使用してください' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+
     validates :date_of_birth
   end
 
