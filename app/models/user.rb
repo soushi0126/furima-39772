e2083a6a18e3bef484ee39ffb5c64b2d1de519ce
soushi0126 <sousi0126@gmail.com>
2024-01-09 23:41:29 +0900

    パスワードに関するバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
     validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
+  with_options presence: true do
     validates :nickname
-
+    validates :date_of_birth
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
       validates :last_name
       validates :first_name
@@ -18,7 +18,5 @@ class User < ApplicationRecord
       validates :last_name_kana
       validates :first_name_kana
     end
-
-    validates :date_of_birth
   end
 end
