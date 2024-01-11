3711f89ae1a9bfeda7b000c16fc9a1412c9226d5
soushi0126 <sousi0126@gmail.com>
2024-01-11 16:20:29 +0900

    バリデーションが間違えられていたので修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,9 +17,9 @@ class Item < ApplicationRecord
   # バリデーション設定
   with_options presence: true do
     validates :name
+    validates :description
     validates :price, numericality: { in: 300..9999999 }
     with_options numericality: { other_than: 1, message: "can't be blank" } do
-      validates :description
       validates :category_id
       validates :situation_id
       validates :delivery_load_id
