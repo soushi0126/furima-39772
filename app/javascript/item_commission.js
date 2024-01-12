1a335545e6b1e2285b1f4b4a3f080c682bc3f03a
soushi0126 <sousi0126@gmail.com>
2024-01-12 17:04:41 +0900

    JavaScriptにて手数料と販売利益を出力

---
+++ app/javascript/item_commission.js
@@ -0,0 +1,11 @@
+function item_commission (){
+  const inputPrice = document.getElementById("item-price");
+  inputPrice.addEventListener('input', () => {
+    const outputCommission = document.getElementById("add-tax-price");
+    const outputprofit = document.getElementById("profit");
+    outputCommission.innerHTML = Math.round(inputPrice.value * 0.1)
+    outputprofit.innerHTML = Math.round(inputPrice.value - Math.round(inputPrice.value * 0.1))
+  })
+};
+
+window.addEventListener('turbo:load', item_commission)
