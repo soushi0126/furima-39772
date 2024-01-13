91267ee2295a252277736647e61ad0f309e9c3dc
soushi0126 <sousi0126@gmail.com>
2024-01-13 12:43:49 +0900

    一部バリーデーションを変更・修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,9 +18,9 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price, numericality: { in: 300..9999999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999 }
     validates :image
-    with_options numericality: { other_than: 1, message: "can't be blank" } do
+    with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :situation_id
       validates :delivery_load_id
