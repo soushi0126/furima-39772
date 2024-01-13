ad6dde028f509f3de6ac1dce68c19973f11eabf1
soushi0126 <sousi0126@gmail.com>
2024-01-13 17:55:15 +0900

    整数でないと出品できないようにバリデーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
     with_options numericality: { other_than: 1 } do
       validates :category_id
