d5a373a8bc1838369b2e61b7a1d861d93e9a7ae4
soushi0126 <sousi0126@gmail.com>
2024-01-17 13:54:40 +0900

    商品編集機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  # before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,12 +20,33 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+
+  end
+
+
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :situation_id, :delivery_load_id, :prefecture_id,
                                  :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
