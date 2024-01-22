ebb6253e2776806f4afc058ac244264cfb4788c9
soushi0126 <sousi0126@gmail.com>
2024-01-22 19:22:29 +0900

    入力に間違いがあればエラーメッセージが出力されるよう実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,24 +1,31 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
   end
-  
+
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
       @purchase_address.save
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def purchase_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipalities, 
-                                              :street_address, :building, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(
+      :post_code, :prefecture_id, :municipalities, 
+      :street_address, :building, :telephone
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
