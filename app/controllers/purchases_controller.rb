aa0f09c2ae1660ed60a25186030bd6b6c2648e51
soushi0126 <sousi0126@gmail.com>
2024-01-25 15:31:43 +0900

    決算処理を実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,14 +4,17 @@ class PurchasesController < ApplicationController
 
   def index
     @purchase_address = PurchaseAddress.new
+    set_payjp_public_key
   end
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
+      set_payjp_public_key
       render :index, status: :unprocessable_entity
     end
   end
@@ -22,10 +25,23 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
   def purchase_params
     params.require(:purchase_address).permit(
       :post_code, :prefecture_id, :municipalities, 
       :street_address, :building, :telephone
-    ).merge(user_id: current_user.id, item_id: params[:item_id])
+    ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_payjp_public_key
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,10 +1,11 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipalities, :street_address, :building, :telephone
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipalities, :street_address, :building, :telephone, :token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :municipalities
